73b4600112bd10e13e4035b038b9884c87130781
Junhua Zhang <2414890918@qq.com>
2017-08-20 21:15:43 -0700

    changed some variable names and refactored the initial simcount process

--- src/pregelplus_similation_do.h
+++ src/pregelplus_similation_do.h
@@ -7,44 +7,32 @@ typedef unsigned char UINT_8;
 
 //Query graph
 struct Query_Graph {
-	vector<char> label;
-	vector<vector<VertexID> > Q_id;
+	vector<char> labels;
+	vector<vector<VertexID> > queryVertexToEdges;
 };
 
-static Query_Graph graph;
-static VertexID sum_num = 0;
+static Query_Graph q;
 
-class init_Query {
-public:
-	init_Query() {
-		graph.label.push_back('a');
-		graph.label.push_back('b');
-		graph.label.push_back('c');
-		graph.label.push_back('d');
-		vector<VertexID> temp;
-		temp.push_back(1);
-		temp.push_back(2);
-		graph.Q_id.push_back(temp);
-		temp.clear();
-		temp.push_back(2);
-		graph.Q_id.push_back(temp);
-		temp.clear();
-		temp.push_back(3);
-		graph.Q_id.push_back(temp);
-		temp.clear();
-		temp.push_back(1);
-		graph.Q_id.push_back(temp);
-		temp.clear();
-
-		for (int i = 0; i < graph.label.size(); i++) {
-			printf("label=%c Q_id=%d edge=", graph.label[i], i);
-			for (int j = 0; j < graph.Q_id[i].size(); j++) {
-				printf("%d ", graph.Q_id[i][j]);
-			}
-			printf("\n");
-		}
-	}
-};
+void init_Query() {
+	q.labels.push_back('a');
+	q.labels.push_back('b');
+	q.labels.push_back('c');
+	q.labels.push_back('d');
+	vector<VertexID> temp;
+	temp.push_back(1);
+	temp.push_back(2);
+	q.queryVertexToEdges.push_back(temp);
+	temp.clear();
+	temp.push_back(2);
+	q.queryVertexToEdges.push_back(temp);
+	temp.clear();
+	temp.push_back(3);
+	q.queryVertexToEdges.push_back(temp);
+	temp.clear();
+	temp.push_back(1);
+	q.queryVertexToEdges.push_back(temp);
+	temp.clear();
+}
 
 UINT_8 GetBit(UINT_32 number, UINT_32 index) {
 	if (index < 0 || index > 31)
@@ -54,59 +42,63 @@ UINT_8 GetBit(UINT_32 number, UINT_32 index) {
 
 struct CCValue_pregel {
 	char label;
-	int D_id;
-	int outedge_num;
-	vector<VertexID> edges; //邻接顶点
-	vector<VertexID> simulate; //子结点simulate数[*,*,*....]
-	vector<VertexID> can_simulate; //自身simulate结点{*,*...}
+	int id;
+	int inDegree;
+	vector<VertexID> outNeighbors; //邻接顶点
+	vector<VertexID> simcount; //子结点simulate数[*,*,*....]
+	vector<VertexID> simset; //自身simulate结点{*,*...}
 };
 
 ibinstream & operator<<(ibinstream & m, const CCValue_pregel & v) {
 	m << v.label;
-	m << v.D_id;
-	m << v.outedge_num;
-	m << v.edges;
-	m << v.simulate;
-	m << v.can_simulate;
+	m << v.id;
+	m << v.inDegree;
+	m << v.outNeighbors;
+	m << v.simcount;
+	m << v.simset;
 	return m;
 }
 
 obinstream & operator>>(obinstream & m, CCValue_pregel & v) {
 	m >> v.label;
-	m >> v.D_id;
-	m >> v.outedge_num;
-	m >> v.edges;
-	m >> v.simulate;
-	m >> v.can_simulate;
+	m >> v.id;
+	m >> v.inDegree;
+	m >> v.outNeighbors;
+	m >> v.simcount;
+	m >> v.simset;
 	return m;
 }
 
 class CCVertex_pregel: public Vertex<VertexID, CCValue_pregel, int> {
 public:
 	void broadcast(int msg) {
-		vector<VertexID> & nbs = value().edges;
+		vector<VertexID> & nbs = value().outNeighbors;
 		for (int i = 0; i < nbs.size(); i++) {
 			send_message(nbs[i], msg);
 		}
 	}
 
 	virtual void compute(MessageContainer & messages) {
-		if (step_num() == 1) //超步
-				{
-			int trans_messages = 0x00;
-			for (int j = 0; j < graph.label.size(); j++) {
-				value().simulate.push_back(value().outedge_num);
+		if (step_num() == 1) {
+			/*
+			 * initial the simcount array, the size of this array equals to the number of vertex
+			 */
+			for (int j = 0; j < q.labels.size(); j++) {
+				value().simcount.push_back(value().inDegree);
 			}
 
-			//print sum_num
-			vector<VertexID> broad;
+			/*
+			 * setup the bitmap message
+			 */
+			int trans_messages = 0x00;
 			bool can_sim = false;
-			for (int i = 0; i < graph.Q_id.size(); i++) {
+			for (int i = 0; i < q.labels.size(); i++) {
 				can_sim = false;
-				vector<VertexID> &nps = value().can_simulate;
+				vector<VertexID> &nps = value().simset;
 				for (int j = 0; j < nps.size(); j++) {
-					if (i == nps[j])
+					if (i == nps[j]){
 						can_sim = true;
+					}
 				}
 				if (!can_sim) {
 					int tmp = 1;
@@ -116,13 +108,36 @@ public:
 					trans_messages += tmp;
 				}
 			}
-			printf("trans_messages2=%d \n", trans_messages);
+
+			/*
+			 * refactoring the bitmap msg setup
+			 */
+			int bitmap_msg=0x0;
+			for(int i=0;i<q.labels.size();i++){
+				if(q.labels[i]!=value().label){
+					bitmap_msg|=1<<i;
+				}
+			}
+			/*
+			 * print the bitmap message
+			 */
+			printf("bitmap_msg from v %d is %d:", value().id,trans_messages);
 			for (int i = 31; i >= 0; i--) {
 				printf("%d", GetBit(trans_messages, i));
-				if (i % 4 == 0)
+				if (i % 8 == 0)
 					putchar(' ');
 			}
-			broadcast(trans_messages);
+			printf("\n");
+
+			printf("bitmap_msg from v %d is %d:", value().id,bitmap_msg);
+			for (int i = 31; i >= 0; i--) {
+				printf("%d", GetBit(bitmap_msg, i));
+				if (i % 8 == 0)
+					putchar(' ');
+			}
+			printf("\n");
+
+			broadcast(bitmap_msg);
 			vote_to_halt();
 		} else {
 			int trans_messages = 0x00;
@@ -131,21 +146,22 @@ public:
 			vector<VertexID>::iterator iter;
 			for (iter = messages.begin(); iter != messages.end(); iter++) {
 				int message = *iter;
-				for (int i = value().simulate.size(); i >= 0; i--) {
+				for (int i = value().simcount.size(); i >= 0; i--) {
 					int minus = GetBit(message, i);
 					if (minus == 1)
-						value().simulate[i]--;
+						value().simcount[i]--;
 				}
 			}
 
 			bool can_sim = true;
-			for (int j = 0; j < value().can_simulate.size(); j++) {
+			for (int j = 0; j < value().simset.size(); j++) {
 				can_sim = true;
-				int judge = value().can_simulate[j];
+				int judge = value().simset[j];
 				if (judge != 404) {
-					for (int p = 0; p < graph.Q_id[judge].size(); p++) {
-						int j = graph.Q_id[judge][p];
-						if (value().simulate[j] <= 0)
+					for (int p = 0; p < q.queryVertexToEdges[judge].size();
+							p++) {
+						int j = q.queryVertexToEdges[judge][p];
+						if (value().simcount[j] <= 0)
 							can_sim = false;
 					}
 				}
@@ -155,21 +171,21 @@ public:
 						tmp = tmp * 2;
 					}
 					trans_messages += tmp;
-					value().can_simulate[j] = 404;
+					value().simset[j] = 404;
 				}
 			}
 
 			vector<VertexID>::iterator iter4;
-			printf("id=:%d\t simulate:", value().D_id);
-			for (iter4 = value().simulate.begin();
-					iter4 != value().simulate.end(); iter4++) {
+			printf("id=:%d\t simulate:", value().id);
+			for (iter4 = value().simcount.begin();
+					iter4 != value().simcount.end(); iter4++) {
 				VertexID tmp4 = *iter4;
 				printf("%d ", tmp4);
 			}
 			printf("after can_simulate:  ");
 			vector<VertexID>::iterator iter2;
-			for (iter2 = value().can_simulate.begin();
-					iter2 != value().can_simulate.end(); iter2++) {
+			for (iter2 = value().simset.begin(); iter2 != value().simset.end();
+					iter2++) {
 				VertexID tmp2 = *iter2;
 				printf("%d ", tmp2);
 			}
@@ -186,21 +202,21 @@ class CCWorker_pregel: public Worker<CCVertex_pregel> {
 	char buf[100];
 
 public:
-	virtual CCVertex_pregel* toVertex(char* line){
+	virtual CCVertex_pregel* toVertex(char* line) {
 		char * pch;
 		pch = strtok(line, "\t");
 		CCVertex_pregel* v = new CCVertex_pregel;
 		v->id = atoi(pch); //v->id保存制表符（/t）之前的数据
-		v->value().D_id = v->id;
+		v->value().id = v->id;
 		pch = strtok(NULL, " ");
 		char* label = pch; //label保存顶点种类
 		v->value().label = label[0];
 		vector<char>::iterator iter1;
 		int p = 0;
-		for (iter1 = graph.label.begin(); iter1 != graph.label.end(); iter1++) {
+		for (iter1 = q.labels.begin(); iter1 != q.labels.end(); iter1++) {
 			char tmp1 = *iter1;
 			if (tmp1 == label[0]) {
-				v->value().can_simulate.push_back(p);
+				v->value().simset.push_back(p);
 			}
 			p++;
 		}
@@ -209,18 +225,17 @@ public:
 		int num = atoi(pch); //num保存邻接点个数
 		for (int i = 0; i < num; i++) {
 			pch = strtok(NULL, " ");
-			v->value().edges.push_back(atoi(pch)); //v->value保存每个邻接顶点的ID
+			v->value().outNeighbors.push_back(atoi(pch)); //v->value保存每个邻接顶点的ID
 		}
 		pch = strtok(NULL, " "); //在邻接点后面插入顶点出度
-		v->value().outedge_num = atoi(pch);
+		v->value().inDegree = atoi(pch);
 
-		sum_num++;
 		return v;
 	}
 
 	virtual void toline(CCVertex_pregel* v, BufferedWriter & writer) {
-		sprintf(buf, "vid:%d\t can_similate:%d \n", v->value().D_id,
-				v->value().can_simulate[0]);
+		sprintf(buf, "vid:%d\t can_similate:%d \n", v->value().id,
+				v->value().simset[0]);
 		writer.write(buf);
 	}
 };
@@ -246,7 +261,7 @@ void pregel_similation(string in_path, string out_path, bool use_combiner) {
 	param.native_dispatcher = false;
 	CCWorker_pregel worker;
 	CCCombiner_pregel combiner;
-	if (use_combiner)
-		worker.setCombiner(&combiner);
+//	if (use_combiner)
+//		worker.setCombiner(&combiner);
 	worker.run(param);
 }
