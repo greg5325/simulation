8ccd0536ca7a550880c4a010a1ed98f5f69a215c
Junhua Zhang <2414890918@qq.com>
2018-01-21 04:50:03 -0800

    add leastDualMatchCount

--- src/basic/SelfSimulation.h
+++ src/basic/SelfSimulation.h
@@ -4,7 +4,8 @@
 #include <vector>
 using namespace std;
 
-vector<unsigned int> selfsimulation(vector<char> & labels, vector<vector<int> > & queryVertexToEdges) {
+vector<unsigned int> selfsimulation(vector<char> & labels,
+		vector<vector<int> > & queryVertexToEdges) {
 	vector<unsigned int> prevsims;
 	vector<unsigned int> sims;
 	vector<unsigned int> removes;
@@ -119,9 +120,8 @@ vector<unsigned int> selfsimulation(vector<char> & labels, vector<vector<int> > 
 	return sims;
 }
 
-
-
-vector<unsigned int> selfdualsimulation(vector<char> & labels, vector<vector<int> > & outEdges) {
+vector<unsigned int> selfdualsimulation(vector<char> & labels,
+		vector<vector<int> > & outEdges) {
 
 	vector<vector<int> > inEdges;
 	inEdges.resize(outEdges.size());
@@ -210,14 +210,10 @@ vector<unsigned int> selfdualsimulation(vector<char> & labels, vector<vector<int
 		removes_suc.push_back(suc_V & (~suc_sim)); //push the value of remove_suc_v
 	}
 
-
 //	vector<unsigned int> prevsims;
 //	vector<unsigned int> sims;
 //	vector<unsigned int> removes_pre;
 //	vector<unsigned int> removes_suc;
-
-
-
 
 	bool exist = false;
 	int v;
@@ -232,39 +228,38 @@ vector<unsigned int> selfdualsimulation(vector<char> & labels, vector<vector<int
 
 		printf("========================while start=======================");
 
-		for(int i=0;i<sims.size();i++){
-			printf("sims of %d:",i);
-			for(int j=0;j<labels.size();j++){
-				if(sims[i] & 1 << j){
-					printf("%d ",j);
+		for (int i = 0; i < sims.size(); i++) {
+			printf("sims of %d:", i);
+			for (int j = 0; j < labels.size(); j++) {
+				if (sims[i] & 1 << j) {
+					printf("%d ", j);
 				}
 			}
 			printf("\n");
 		}
 		printf("=============sims end=============\n\n");
 
-		for(int i=0;i<removes_pre.size();i++){
-			printf("remove_pre of %d:",i);
-			for(int j=0;j<labels.size();j++){
-				if(removes_pre[i] & 1<<j){
-					printf("%d ",j);
+		for (int i = 0; i < removes_pre.size(); i++) {
+			printf("remove_pre of %d:", i);
+			for (int j = 0; j < labels.size(); j++) {
+				if (removes_pre[i] & 1 << j) {
+					printf("%d ", j);
 				}
 			}
 			printf("\n");
 		}
 		printf("===========removes_pre end===========\n\n");
 
-		for(int i=0;i<removes_pre.size();i++){
-			printf("remove_suc of %d:",i);
-			for(int j=0;j<labels.size();j++){
-				if(removes_suc[i] & 1<<j){
-					printf("%d ",j);
+		for (int i = 0; i < removes_pre.size(); i++) {
+			printf("remove_suc of %d:", i);
+			for (int j = 0; j < labels.size(); j++) {
+				if (removes_suc[i] & 1 << j) {
+					printf("%d ", j);
 				}
 			}
 			printf("\n");
 		}
 		printf("===========removes_suc end===========\n\n");
-
 
 		if (removes_pre[v]) {
 			for (unsigned int u = 0; u < labels.size(); u++) {
@@ -295,9 +290,10 @@ vector<unsigned int> selfdualsimulation(vector<char> & labels, vector<vector<int
 									}
 								}
 
-								for (unsigned int w__index = 0; w__index < outEdges[w].size();
+								for (unsigned int w__index = 0;
+										w__index < outEdges[w].size();
 										w__index++) {
-									unsigned int w__=outEdges[w][w__index];
+									unsigned int w__ = outEdges[w][w__index];
 									unsigned int parents = 0;
 
 									for (vector<int>::iterator parent =
@@ -321,11 +317,12 @@ vector<unsigned int> selfdualsimulation(vector<char> & labels, vector<vector<int
 			prevsims[v] = sims[v];
 			removes_pre[v] = 0;
 		} else {
-			for (unsigned int u_index = 0; u_index < outEdges[v].size(); u_index++) { //for all u in suc(v)
-				unsigned int u=outEdges[v][u_index];
+			for (unsigned int u_index = 0; u_index < outEdges[v].size();
+					u_index++) { //for all u in suc(v)
+				unsigned int u = outEdges[v][u_index];
 				for (unsigned int w = 0; w < labels.size(); w++) {
 					if (removes_suc[v] & 1 << w) { //for all w in remove(v)
-							printf("remove %d from sims[%d] of suc(%d)\n",w,u,v);
+						printf("remove %d from sims[%d] of suc(%d)\n", w, u, v);
 						if (sims[u] & 1 << w) { //w simulate u
 							sims[u] &= ~(1 << w);
 
@@ -334,8 +331,7 @@ vector<unsigned int> selfdualsimulation(vector<char> & labels, vector<vector<int
 								unsigned int childs = 0;
 								for (vector<int>::iterator child =
 										outEdges[w__].begin();
-										child != outEdges[w__].end();
-										child++) {
+										child != outEdges[w__].end(); child++) {
 									childs |= 1 << *child;
 								}
 								if (childs & 1 << w) { //for all w__ in pre(w)
@@ -345,9 +341,9 @@ vector<unsigned int> selfdualsimulation(vector<char> & labels, vector<vector<int
 								}
 							}
 
-							for (unsigned int w__index = 0; w__index < outEdges[w].size();
-									w__index++) {
-								unsigned int w__=outEdges[w][w__index];
+							for (unsigned int w__index = 0;
+									w__index < outEdges[w].size(); w__index++) {
+								unsigned int w__ = outEdges[w][w__index];
 								unsigned int parents = 0;
 
 								for (vector<int>::iterator parent =
@@ -367,8 +363,8 @@ vector<unsigned int> selfdualsimulation(vector<char> & labels, vector<vector<int
 					}
 				}
 			}
-			prevsims[v]=sims[v];
-			removes_suc[v]=0;
+			prevsims[v] = sims[v];
+			removes_suc[v] = 0;
 		}
 
 		exist = false;
@@ -398,7 +394,8 @@ vector<int> leastMatchCount(vector<unsigned int> & sims) {
 	return least_match_counts;
 }
 
-vector<vector<int> > leastMatchCount2(vector<char> & labels, vector<vector<int> > & queryVertexToEdges){
+vector<vector<int> > leastMatchCount2(vector<char> & labels,
+		vector<vector<int> > & queryVertexToEdges) {
 	vector<unsigned int> prevsims;
 	vector<unsigned int> sims;
 	vector<unsigned int> removes;
@@ -510,15 +507,16 @@ vector<vector<int> > leastMatchCount2(vector<char> & labels, vector<vector<int> 
 		}
 	}
 
-
 	vector<vector<int> > least_match_counts;
 	least_match_counts.resize(queryVertexToEdges.size());
 
-	for(unsigned int src=0;src<queryVertexToEdges.size();src++){
-		least_match_counts[src].resize(queryVertexToEdges.size(),0);
-		for(unsigned int dst=0;dst<queryVertexToEdges[src].size();dst++){
-			for(unsigned int i=0;i<queryVertexToEdges[src].size();i++){
-				if(sims[queryVertexToEdges[src][dst]] & 1<<queryVertexToEdges[src][i]){
+	for (unsigned int src = 0; src < queryVertexToEdges.size(); src++) {
+		least_match_counts[src].resize(queryVertexToEdges.size(), 0);
+		for (unsigned int dst = 0; dst < queryVertexToEdges[src].size();
+				dst++) {
+			for (unsigned int i = 0; i < queryVertexToEdges[src].size(); i++) {
+				if (sims[queryVertexToEdges[src][dst]]
+						& 1 << queryVertexToEdges[src][i]) {
 					least_match_counts[src][queryVertexToEdges[src][dst]]++;
 				}
 			}
@@ -527,7 +525,57 @@ vector<vector<int> > leastMatchCount2(vector<char> & labels, vector<vector<int> 
 	return least_match_counts;
 }
 
+vector<vector<vector<int> > > leastDualMatchCount(vector<char> & labels,
+		vector<vector<int> > & outEdges) {
+	vector<vector<int> > inEdges;
+	inEdges.resize(outEdges.size());
+	for (int i = 0; i < outEdges.size(); i++) {
+		for (int j = 0; j < outEdges[i].size(); j++) {
+			inEdges[outEdges[i][j]].push_back(i);
+		}
+	}
+
+	vector<unsigned int> sims=selfdualsimulation(labels,outEdges);
 
 
+	vector<vector<int> > least_pre_match_counts;
+	least_pre_match_counts.resize(outEdges.size());
+
+	for (unsigned int src = 0; src < outEdges.size(); src++) {
+		least_pre_match_counts[src].resize(outEdges.size(), 0);
+		for (unsigned int dst = 0; dst < outEdges[src].size();
+				dst++) {
+			for (unsigned int i = 0; i < outEdges[src].size(); i++) {
+				if (sims[outEdges[src][dst]]
+						& 1 << outEdges[src][i]) {
+					least_pre_match_counts[src][outEdges[src][dst]]++;
+				}
+			}
+		}
+	}
+
+
+	vector<vector<int> > least_suc_match_counts;
+	least_suc_match_counts.resize(inEdges.size());
+
+	for (unsigned int src = 0; src < inEdges.size(); src++) {
+		least_suc_match_counts[src].resize(inEdges.size(), 0);
+		for (unsigned int dst = 0; dst < inEdges[src].size();
+				dst++) {
+			for (unsigned int i = 0; i < inEdges[src].size(); i++) {
+				if (sims[inEdges[src][dst]]
+						& 1 << inEdges[src][i]) {
+					least_suc_match_counts[src][inEdges[src][dst]]++;
+				}
+			}
+		}
+	}
+
+	vector<vector<vector<int> > > least_match_counts;
+	least_match_counts.push_back(least_pre_match_counts);
+	least_match_counts.push_back(least_suc_match_counts);
+
+	return least_match_counts;
+}
 
 #endif
